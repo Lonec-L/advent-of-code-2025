f38c33a4fe7b5e8282c64bae23d48f3a76ba4ccb
Luka Lonec <luka.lonec@gmail.com>
2025-12-06 13:55:34 +0100

    feat: day 5 part 2

--- src/5.c
+++ src/5.c
@@ -17,9 +17,9 @@ int main()
 		n++;
 	}
 
+	// pt1
 	long long x;
 	int res = 0;
-	long long res2 = 0;
 	while (fscanf(in_file, "%lli\n", &x) != EOF) {
 		for (int i = 0; i < n; i++) {
 			if (x >= limits[i][0] && x <= limits[i][1]) {
@@ -27,6 +27,35 @@ int main()
 				break;
 			}
 		}
+	}
+	// pt2
+	long long res2 = 0;
+	for (int i = 0; i < n; i++) {
+		if (limits[i][0] == -1 && limits[i][1] == -1) {
+			continue;
+		}
+		for (int j = i + 1; j < n; j++) {
+			if (limits[i][0] == -1 && limits[i][1] == -1) {
+				continue;
+			}
+			if (limits[i][0] > limits[j][1] || limits[i][1] < limits[j][0]) {
+				continue;
+			} else if (limits[i][0] >= limits[j][0] && limits[i][1] <= limits[j][1]) {
+				limits[i][0] = -1;
+				limits[i][1] = -1;
+			} else if (limits[i][0] <= limits[j][0] && limits[i][1] >= limits[j][1]) {
+				limits[j][0] = -1;
+				limits[j][1] = -1;
+			} else if (limits[i][0] <= limits[j][0] && limits[i][1] < limits[j][1]) {
+				limits[j][0] = limits[i][1] + 1;
+			} else if (limits[i][1] >= limits[j][1] && limits[i][0] > limits[j][0]) {
+				limits[j][1] = limits[i][0] - 1;
+			}
+		}
+		if (limits[i][0] == -1 && limits[i][1] == -1) {
+			continue;
+		}
+		res2 += limits[i][1] - limits[i][0] + 1;
 	}
 	printf("pt1: %i\n", res);
 	printf("pt2: %lli\n", res2);
