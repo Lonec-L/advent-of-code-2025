14d632578e64b3932e876364429403e5eef0aa7d
Luka Lonec <luka.lonec@gmail.com>
2026-01-10 22:40:02 +0100

    misc: WIP

--- src/10.c
+++ src/10.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -56,9 +58,128 @@ int push_light_queue(struct light_queue *q, uint16_t state, int steps)
 	return 0;
 }
 
-// looks like a classic BFS problem. We can present the state as bitmask and then present all
-// operations as bitmasks too. the just BFS from initial state, taking each operation once in each
-// step and seeing where it takes us. If we reach state 0, we found the solution
+void visualize(const int equiations[10][15])
+{
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 14; j++) {
+			printf("%i ", equiations[i][j]);
+		}
+		printf("| %i\n", equiations[i][14]);
+	}
+}
+
+void gauss_jordan(int equations[10][15])
+{
+	int curr_row = 0;
+	for (int i = 0; i < 15; i++) {
+		// find pivot
+		int pivot = -1;
+		for (int j = curr_row; j < 10; j++) {
+			if (equations[j][i] != 0) {
+				pivot = j;
+				break;
+			}
+		}
+		if (pivot == -1) {
+			continue;
+		}
+		// swap rows
+		if (pivot != curr_row) {
+			int tmp;
+			for (int j = 0; j < 15; j++) {
+				tmp = equations[pivot][j];
+				equations[pivot][j] = equations[curr_row][j];
+				equations[curr_row][j] = tmp;
+			}
+		}
+
+		// subtract current row from all others that are below
+		for (int j = curr_row + 1; j < 10; j++) {
+			int factor = (equations[curr_row][i] / equations[j][i]);
+			for (int k = 0; k < 15; k++) {
+				equations[j][k] -= equations[curr_row][k] * factor;
+			}
+		}
+		curr_row++;
+	}
+
+	// jordan part
+	for (int i = 1; i < 10; i++) {
+		// find the pivot
+		int pivot = -1;
+		for (int j = 0; j < 14; j++) {
+			if (equations[i][j] != 0) {
+				if (equations[i][j] != 1) {
+					break;
+				}
+				pivot = j;
+				break;
+			}
+		}
+		if (pivot == -1) {
+			continue;
+		}
+
+		for (int j = 0; j < i; j++) {
+			for (int k = 0; k < 15; k++) {
+				equations[j][k] -= equations[i][k] *
+						   (equations[j][pivot] / equations[i][pivot]);
+			}
+		}
+	}
+}
+
+int solve_pt2(int equations[10][15])
+{
+	// while we have variables to solve, find the column with most ones
+	int res = 0;
+	while (1) {
+		int m_ones = 0;
+		int m_index = -1;
+		for (int i = 0; i < 14; i++) {
+			int tmp = 0;
+			for (int j = 0; j < 10; j++) {
+				if (equations[j][i] != 0) {
+					tmp++;
+				}
+			}
+			if (tmp > m_ones) {
+				m_index = i;
+				m_ones = tmp;
+			}
+		}
+		if (m_ones == 0) {
+			break;
+		}
+		printf("m_index: %i\nm_ones: %i\n", m_index, m_ones);
+
+		// find factor
+		int factor = INT_MAX;
+		for (int i = 0; i < 10; i++) {
+			if (equations[i][m_index] != 0) {
+				int tmp = equations[i][14] / equations[i][m_index];
+				factor = (abs(factor) < abs(tmp)) ? factor : tmp;
+			}
+		}
+		printf("factor: %i\n", factor);
+
+		for (int i = 0; i < 10; i++) {
+			if (factor != INT_MAX) {
+				equations[i][14] -= factor * equations[i][m_index];
+			}
+			equations[i][m_index] = 0;
+		}
+		res += factor;
+		visualize(equations);
+		getchar();
+	}
+	return res;
+}
+
+// pt1 looks like a classic BFS problem. We can present the state as bitmask and then present
+// all operations as bitmasks too. the just BFS from initial state, taking each operation
+// once in each step and seeing where it takes us. If we reach state 0, we found the
+// solution
 int main()
 {
 	FILE *in_file = fopen("in", "r");
@@ -68,8 +189,15 @@ int main()
 	uint16_t operations[20];
 
 	int res1 = 0;
+	int res2 = 0;
+	int max_op_count = 0;
+	int max_joltage_req = 0;
+
+	// 10 equiations with max 14 unknowns and a result in right-most column
+	int equations[10][15] = {0};
 	while (fgets(buffer, sizeof buffer, in_file)) {
 		memset(operations, 0, sizeof(operations));
+		memset(equations, 0, sizeof(equations));
 		uint16_t state = 0;
 		char *ptr = buffer;
 		char *token;
@@ -97,11 +225,14 @@ int main()
 			char *subtoken;
 			while ((subtoken = strsep(&subptr, ",")) != NULL) {
 				operations[op_count] |= (1 << atoi(subtoken));
+				if (atoi(subtoken) < 14) {
+					equations[atoi(subtoken)][op_count] = 1;
+				}
 			}
 			op_count++;
-
 			token = strsep(&ptr, " \n");
 		}
+		max_op_count = max(max_op_count, op_count);
 
 		while (token != NULL && token[0] == '{') {
 			token++;
@@ -111,8 +242,12 @@ int main()
 
 			char *subptr = token;
 			char *subtoken;
+			int jol_req = 0;
 			while ((subtoken = strsep(&subptr, ",")) != NULL) {
+				equations[jol_req][14] = atoi(subtoken);
+				jol_req++;
 			}
+			max_joltage_req = max(max_joltage_req, jol_req);
 
 			token = strsep(&ptr, " \n");
 		}
@@ -146,6 +281,23 @@ int main()
 				perror("failed to pop!\n");
 			}
 		}
+
+		// pt2
+		printf("pt2\n");
+		visualize(equations);
+		// TODO: this is not yet correct!
+		gauss_jordan(equations);
+		printf("\n");
+		visualize(equations);
+		// when we have variables in reduced low echolen form, we want to maximise the free
+		// variables with the most impact, to reduce the number of presses to minimum
+		res2 += solve_pt2(equations);
+		printf("\n");
+		visualize(equations);
+		printf("\n");
 	}
-	printf("day1: %i\n", res1);
+	printf("pt1: %i\n", res1);
+	printf("pt2: %i\n", res2);
+
+	printf("max_jol: %i\nmax_op: %i\n", max_joltage_req, max_op_count);
 }
---
+++ src/12.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("hello last day!\n");
+}
